28bd8b57d96b5f390e9c5f481657f900a4eba372
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-10-18 15:49:05 +0545

    some of the ui are edited

--- resources/views/Layout/navbar.blade.php
+++ resources/views/Layout/navbar.blade.php
@@ -1,4 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-light bg-light">
+<nav class=" navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-light bg-light">
+  <div class="container">
     <a class="navbar-brand" href="#"><img src="https://instagram.fktm1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/71525760_1082127131981111_1826169740062621696_n.jpg?_nc_ht=instagram.fktm1-2.fna.fbcdn.net&_nc_ohc=oZWZgJucwpEAX8XG5M3&oh=774729ad628974305e010cc6df16f28d&oe=5FB4CE09" alt="Logo for Designs by Yogesh" height="70px"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -19,4 +20,6 @@
           </li>
       </ul>
     </div>
+  </div>
+   
   </nav>
